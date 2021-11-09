f7560b9432085405d3e4b274c56c09d24facc050
CriSFV <cristina.f.valverde@gmail.com>
2021-11-09 10:21:51 +0100

    about me styles

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,23 +1,46 @@
+import '../styles/AboutMe.scss';
 const AboutMe = () => {
   return (
-    <>
+    <section className='aboutMe'>
       <h1 id='about_me'>Cónoceme</h1>
-
-      <p>
-        Estudié dos años de Educación Social hasta que, haciendo las prácticas
-        de la carrera, vi que no era lo mio, así que me puse a trabajar en el
-        sector de atención al cliente, donde mayoritariamente he estado
-        gestionando equipos en diferentes campañas y a diferentes niveles.
-      </p>
-      <p>Tras unos cuantos años allí. me dí cuenta de que yo quería cambiar</p>
-      <p>
-        Siempre me había llamado la atención el tema de la programación aunque
-        pensaba que ya sería un poco tarde para dar un cambio en mi vida pero al
-        final, tras dejarme aconsejar por gente que me conocía sobre mis
-        expectativas encontré Adalab y tras realizar su bootcamp, aquí estoy, Jr
-        Front-End Dev con muchas ganas de encontrar su sitio en el mundo tech.
-      </p>
-    </>
+      <article>
+        <h3>Esta soy yo</h3>
+        <p>
+          Estudié dos años de Educación Social hasta que, haciendo las prácticas
+          de la carrera, vi que no era lo mio, así que me puse a trabajar en el
+          sector de atención al cliente, donde mayoritariamente he estado
+          gestionando equipos en diferentes campañas y a diferentes niveles.
+        </p>
+        <p>
+          Tras unos cuantos años allí. me dí cuenta de que yo quería cambiar
+        </p>
+        <p>
+          Siempre me había llamado la atención el tema de la programación aunque
+          pensaba que ya sería un poco tarde para dar un cambio en mi vida pero
+          al final, tras dejarme aconsejar por gente que me conocía sobre mis
+          expectativas encontré Adalab y tras realizar su bootcamp, aquí estoy,
+          Jr Front-End Dev encontrando su sitio en el mundo tech.
+        </p>
+      </article>
+      <article>
+        <h2>Mis skills</h2>
+        <ul className='skill__list'>
+          <li className='skill__list__item'>HTML5</li>
+          <li className='skill__list__item'>SASS</li>
+          <li className='skill__list__item'>CSS3</li>
+          <li className='skill__list__item'>JavaScript</li>
+          <li className='skill__list__item'>React</li>
+          <li className='skill__list__item'>NodeJs</li>
+          <li className='skill__list__item'>Express</li>
+          <li className='skill__list__item'>SQLite</li>
+          <li className='skill__list__item'>Git</li>
+          <li className='skill__list__item'>GitHub</li>
+          <li className='skill__list__item'>Slack</li>
+          <li className='skill__list__item'>VsCode</li>
+          <li className='skill__list__item'>Terminal</li>
+        </ul>
+      </article>
+    </section>
   );
 };
 

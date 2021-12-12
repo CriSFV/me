dbb97e33e36dda20efdd81e5f2b20255e47429d8
CriSFV <cristina.f.valverde@gmail.com>
2021-12-12 12:51:24 +0100

    add cv style

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,16 +1,15 @@
 const CvPdf = () => {
   return (
-    <article id='cv' className='cv'>
-      <h3 className='cv__title'>CV</h3>
+    <article className='cv'>
       <a
-        className='cv__download'
+        className=' cv__download'
         href='https://drive.google.com/file/d/1lB7RCwTI4ZnGNn_1OKQAL8ra4UilWFGV/view?usp=sharing'
         target='_blank'
         rel='noreferrer'
         title='CV en PDF'
       >
-        {' '}
-        Descargar <i className='fas fa-download'></i>
+        <h3 className='cv__title'>Curriculum</h3>
+        <i className='fas fa-download'></i>{' '}
       </a>
     </article>
   );

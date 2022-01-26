06221d4050a61e8c7529617352059de01b5f3a2a
CriSFV <cristina.f.valverde@gmail.com>
2022-01-26 16:02:20 +0100

    change blue

--- src/components/ProjectsFilter.js
+++ src/components/ProjectsFilter.js
@@ -5,23 +5,23 @@ export const ProjectsFilter = (props) => {
   return (
     <>
       <label htmlFor='projects' className='filter__project'>
-        ¿Quieres ver algo en particular?:{' '}
+        {' '}
+        <select
+          className='filter__projects__select tags'
+          name='projects'
+          id='projects'
+          value={props.userSearch}
+          onChange={optionValue}
+        >
+          <option value='all'>Todos</option>
+          <option value='HTML5'>HTML5</option>
+          <option value='CSS3'>CSS3</option>
+          <option value='SASS'>SASS</option>
+          <option value='JAVASCRIPT'>JavaScript</option>
+          <option value='REACT'>ReactJS</option>
+          <option value='NODEJS'>NodeJS</option>
+        </select>
       </label>
-      <select
-        className='filter__projects__select tags'
-        name='projects'
-        id='projects'
-        value={props.userSearch}
-        onChange={optionValue}
-      >
-        <option value='all'>Todos</option>
-        <option value='HTML5'>HTML5</option>
-        <option value='CSS3'>CSS3</option>
-        <option value='SASS'>SASS</option>
-        <option value='JAVASCRIPT'>JavaScript</option>
-        <option value='REACT'>ReactJS</option>
-        <option value='NODEJS'>NodeJS</option>
-      </select>
     </>
   );
 };
